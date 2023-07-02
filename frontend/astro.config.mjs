7c8b512b80032ae6a2c02b2089621fbe112c2b46
kazmi066 <awaisabbas47@gmail.com>
2023-07-02 19:50:15 +0500

    Add sanity studio content

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,7 +5,7 @@ import sanity from "astro-sanity";
 // https://astro.build/config
 export default defineConfig({
   integrations: [sanity({
-    projectId: '8v4lj4cg',
+    projectId: '',
     dataset: 'production',
     apiVersion: '2023-02-08',
     useCdn: false,
